be553e0ae6ce2cd7ca2db79055beb0b8d580a141
David Asselborn <David@Davids-MacBook.local>
2018-01-10 14:46:48 +0100

    Add search functionality.

--- InformatikzentrumGuide/View Controllers/SearchTableViewController.swift
+++ InformatikzentrumGuide/View Controllers/SearchTableViewController.swift
@@ -12,11 +12,14 @@ import CoreData
 
 class SearchTableViewController: UITableViewController {
     
-    var places: [NSManagedObject] = []
+    // All places fetched from Core Data
+    var places: [Place] = []
+    // Places matching the search, at beginning containing also all places
+    var filteredPlaces: [Place] = []
     
     override func viewDidLoad() {
         
-        // Do ONCE to get it into Core Data for testing, else you get duplicates
+        // Do ONCE to get it into Core Data for testing, otherwise you get duplicates
 //        self.save(name: "Mensa", latitude: 50.7795, longitude: 6.0595)
 //        self.save(name: "Aula 2", latitude: 50.7794, longitude: 6.0585)
 //        self.save(name: "Fachschaft", latitude: 50.7790, longitude: 6.0591)
@@ -34,10 +37,12 @@ class SearchTableViewController: UITableViewController {
         let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Place")
 
         do {
-            places = try managedContext.fetch(fetchRequest)
+            places = try managedContext.fetch(fetchRequest) as! [Place]
         } catch let error as NSError {
             print("Could not fetch. \(error), \(error.userInfo)")
         }
+        
+        filteredPlaces = places
     }
 
     // Used to enter data into persistent storage
@@ -55,31 +60,47 @@ class SearchTableViewController: UITableViewController {
 
         do {
             try managedContext.save()
-            places.append(place)
+            places.append(place as! Place)
         } catch let error as NSError {
             print("Could not save. \(error), \(error.userInfo)")
         }
     }
     
+    // Search for appearance of entered string in any place name
+    func filterContentForSearchText(_ searchText: String, scope: String = "All") {
+        filteredPlaces = places.filter({( place : Place) -> Bool in
+            return (place.name?.lowercased().contains(searchText.lowercased()))!
+        })
+        // Reset filter if no text is entered to show all places
+        if (searchText == "") {
+            filteredPlaces = places
+        }
+        tableView.reloadData()
+    }
 }
 
+// Includes all searchResult delegates
 extension SearchTableViewController: UISearchResultsUpdating {
     func updateSearchResults(for searchController: UISearchController) {
         // Used to display all places when search bar is tapped but no text is entered yet
         searchController.searchResultsController?.view.isHidden = false
+        // Adapt tableView content based on updated search input
+        filterContentForSearchText(searchController.searchBar.text!)
     }
 }
 
+
+// Includes all tableView delegates
 extension SearchTableViewController {
     
     // Filter according to search input
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return places.count
+        return filteredPlaces.count
     }
     
     // At default display all places
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let place = places[indexPath.row]
+            let place = filteredPlaces[indexPath.row]
             let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
             cell.textLabel?.text = place.value(forKeyPath: "name") as? String
             return cell
